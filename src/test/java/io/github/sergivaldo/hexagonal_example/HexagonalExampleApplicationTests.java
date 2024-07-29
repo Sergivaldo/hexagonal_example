58cdf6482f30267e86ceb6c98695bbe618b6e52f
Sergivaldo <sergivaldo123@gmail.com>
2024-07-29 13:27:19 -0300

    init

---
+++ src/test/java/io/github/sergivaldo/hexagonal_example/HexagonalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sergivaldo.hexagonal_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
